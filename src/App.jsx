de6781229a023df5f8106aeeb56ec7dd0c4d59eb
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-09-23 16:11:14 +0000

    Add React Router and Prettier dependencies
    Add base Home and About routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <p>
-        Hello world
-      </p>
-    </>
+    <BrowserRouter>
+      {/* <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav> */}
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
