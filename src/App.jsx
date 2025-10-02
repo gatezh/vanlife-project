5b8f87be3ff33a856081b77a40f7a1c0b5011429
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-10-02 18:28:42 +0000

    Add VanDetail page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
 import "./App.css";
 
 import "../server";
@@ -23,6 +24,7 @@ function App() {
         {/* Fix for GitHub Pages deployment */}
         <Route path="/vanlife-project/" element={<Home />}></Route>
         <Route path="/vans" element={<Vans />}></Route>
+        <Route path="/vans/:id" element={<VanDetail />}></Route>
         <Route path="/about" element={<About />}></Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Vans() {
   const [vans, setVans] = useState([]);
@@ -14,17 +15,22 @@ export default function Vans() {
     <div className="van-list-container">
       <h1>Explore our van options</h1>
       <div className="van-list">
-        {vans.map(van => (
-          <div key={van.id} className="van-tile">
-            <img src={van.imageUrl} />
-            <div className="van-info">
-              <h3>{van.name}</h3>
-              <p>${van.price}<span>/day</span></p>
+        {vans.map((van) => (
+          <Link to={`/vans/${van.id}`}>
+            <div key={van.id} className="van-tile">
+              <img src={van.imageUrl} />
+              <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>
+                  ${van.price}
+                  <span>/day</span>
+                </p>
+              </div>
+              <i className={`van-type ${van.type} selected`}>{van.type}</i>
             </div>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
-  )
+  );
 }
