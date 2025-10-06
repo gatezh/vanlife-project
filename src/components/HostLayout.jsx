434f4973b6b5453e4a4fa6507bb427d74266b637
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-10-06 22:01:59 +0000

    Style host page navigation links

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,12 +1,14 @@
-import { Link, Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom";
 
 export default function HostLayout() {
   return (
     <>
-      <Link to="/host">Dashboard</Link>
-      <Link to="/host/income">Income</Link>
-      <Link to="/host/reviews">Reviews</Link>
-      <Outlet />
+      <nav className="host-nav">
+        <Link to="/host">Dashboard</Link>
+        <Link to="/host/income">Income</Link>
+        <Link to="/host/reviews">Reviews</Link>
+        <Outlet />
+      </nav>
     </>
   );
- }
+}
