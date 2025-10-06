321b87da38c2eff76f6093cda95eb4a791fce52a
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-10-06 21:30:48 +0000

    Add host section, utilize index routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import About from "./pages/About";
 import Vans from "./pages/Vans";
 import VanDetail from "./pages/VanDetail";
 import Layout from "./components/Layout";
+import Dashboard from "./pages/Host/Dashboard";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
+import HostLayout from "./components/HostLayout";
 import "./App.css";
 
 import "../server";
@@ -12,13 +16,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
           {/* Fix for GitHub Pages deployment */}
-          <Route path="/vanlife-project/" element={<Home />}></Route>
-          <Route path="/vans" element={<Vans />}></Route>
-          <Route path="/vans/:id" element={<VanDetail />}></Route>
-          <Route path="/about" element={<About />}></Route>
+          <Route path="/vanlife-project/" element={<Home />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/components/HostLayout.jsx
@@ -0,0 +1,12 @@
+import { Link, Outlet } from "react-router-dom"
+
+export default function HostLayout() {
+  return (
+    <>
+      <Link to="/host">Dashboard</Link>
+      <Link to="/host/income">Income</Link>
+      <Link to="/host/reviews">Reviews</Link>
+      <Outlet />
+    </>
+  );
+ }
