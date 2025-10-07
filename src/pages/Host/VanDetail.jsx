616506a06272ec1b2d80178413b16c3d9b374f07
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-10-07 20:37:14 +0000

    Add Host Vans page

---
+++ src/pages/Host/VanDetail.jsx
@@ -0,0 +1,10 @@
+import {Link} from "react-router-dom"
+
+export default function HostVanDetail() {
+  return (
+    <>
+      <Link to="/host/vans">⬅️ Back to your Vans list</Link>
+      <h1>Host Van Detail page</h1>
+    </>
+  )
+}
