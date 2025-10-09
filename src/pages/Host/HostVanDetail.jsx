d8f357b646c8f801bf68c3e8034764e31c116a58
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-10-09 21:40:39 +0000

    Add Host Van detail navigation

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from "react";
-import { Link, useParams, Outlet } from "react-router-dom";
+import { Link, NavLink, useParams, Outlet } from "react-router-dom";
 
 export default function HostVanDetail() {
   const [hostVan, setHostVan] = useState(null);
   const { id } = useParams();
+
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
 
   useEffect(() => {
     fetch(`/api/host/vans/${id}`)
@@ -31,6 +37,28 @@ export default function HostVanDetail() {
             <h4>${hostVan.price}/day</h4>
           </div>
         </div>
+
+        <nav className="host-van-detail-nav">
+          <NavLink
+            to="."
+            end
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Details
+          </NavLink>
+          <NavLink
+            to="pricing"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Pricing
+          </NavLink>
+          <NavLink
+            to="photos"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Photos
+          </NavLink>
+        </nav>
         <Outlet />
       </div>
     </section>
