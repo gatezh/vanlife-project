d7a97ec4910d3e94ce21445f103bececfc295291
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-10-21 21:18:27 +0000

    Set search params while preserving other params

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -18,33 +18,49 @@ export default function Vans() {
     ? vans.filter((van) => (van.type === typeFilter ? van : null))
     : vans;
 
+  function handleFilterChange(key, value) {
+    setSearchParams((prevParams) => {
+      if (value === null) {
+        prevParams.delete(key);
+      } else {
+        prevParams.set(key, value);
+      }
+      return prevParams;
+    });
+  }
+
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
       <button
-        onClick={() => setSearchParams({ type: "simple" })}
-        className="van-type simple"
+        onClick={() => handleFilterChange("type", "simple")}
+        className={`van-type simple 
+                        ${typeFilter === "simple" ? "selected" : ""}`}
       >
         Simple
       </button>
       <button
-        onClick={() => setSearchParams({ type: "luxury" })}
-        className="van-type luxury"
+        onClick={() => handleFilterChange("type", "luxury")}
+        className={`van-type luxury 
+                        ${typeFilter === "luxury" ? "selected" : ""}`}
       >
         Luxury
       </button>
       <button
-        onClick={() => setSearchParams({ type: "rugged" })}
-        className="van-type rugged"
+        onClick={() => handleFilterChange("type", "rugged")}
+        className={`van-type rugged 
+                        ${typeFilter === "rugged" ? "selected" : ""}`}
       >
         Rugged
       </button>
-      <button
-        onClick={() => setSearchParams({})}
-        className="van-type clear-filters"
-      >
-        Clear
-      </button>
+      {typeFilter ? (
+        <button
+          onClick={() => setSearchParams({})}
+          className="van-type clear-filters"
+        >
+          Clear
+        </button>
+      ) : null}
       <div className="van-list">
         {displayVans.map((van) => (
           <Link to={`/vans/${van.id}`} key={van.id}>
