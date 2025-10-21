1f5eb01e321ea568d5f1f07873efc23685d72302
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-10-21 21:18:56 +0000

    Add back link to VanDetail page

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 export default function VanDetails() {
   const [van, setVan] = useState(null);
@@ -7,22 +7,30 @@ export default function VanDetails() {
 
   useEffect(() => {
     fetch(`/api/vans/${id}`)
-      .then(response => response.json())
-      .then(data => setVan(data.vans))
+      .then((response) => response.json())
+      .then((data) => setVan(data.vans));
   }, [id]);
 
   return (
     <div className="van-detail-container">
+      <Link to=".." relative="path" className="back-button">
+        &larr; <span>Back to all vans</span>
+      </Link>
+
       {van ? (
         <div className="van-detail">
           <img src={van.imageUrl} />
           <i className={`van-type ${van.type} selected`}>{van.type}</i>
           <h2>{van.name}</h2>
-          <p className="van-price"><span>${van.price}</span>/day</p>
+          <p className="van-price">
+            <span>${van.price}</span>/day
+          </p>
           <p>{van.description}</p>
           <button className="link-button">Rent this van</button>
         </div>
-      ) : <h2>Loading...</h2>}
+      ) : (
+        <h2>Loading...</h2>
+      )}
     </div>
-  )
+  );
 }
