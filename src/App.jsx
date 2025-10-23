c6c0e6e956e363e0a2499021f0418dc54e6941b2
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-10-23 22:40:20 +0000

    Add catch all NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import HostVanDetail from "./pages/Host/HostVanDetail";
 import HostVanInfo from "./pages/Host/HostVanInfo";
 import HostVanPricing from "./pages/Host/HostVanPricing";
 import HostVanPhotos from "./pages/Host/HostVanPhotos";
+import NotFound from "./pages/NotFound";
 import "./App.css";
 
 import "../server";
@@ -44,6 +45,7 @@ function App() {
               <Route path="photos" element={<HostVanPhotos />} />
             </Route>
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
