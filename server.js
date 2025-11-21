1726afc38433bf9af1be6f9d91cdcf05f522743b
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-11-21 19:31:24 +0000

    Fetch vans list from firebase

--- server.js
+++ server.js
@@ -86,6 +86,7 @@ createServer({
     this.namespace = "api";
     this.logging = false;
     // this.timing = 2000  // => mock a 2 second delay in server response
+    this.passthrough("https://firestore.googleapis.com/**");
 
     this.get("/vans", (schema, request) => {
       // return new Response(400, {}, {error: "Error fetching data"})
@@ -118,7 +119,7 @@ createServer({
         return new Response(
           401,
           {},
-          { message: "No user with those credentials found!" }
+          { message: "No user with those credentials found!" },
         );
       }
 
